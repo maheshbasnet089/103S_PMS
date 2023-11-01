2124bfdd9c6f702047e15fdbbb546372c1dc2f01
maheshbasnet089 <basnetmanish089@gmail.com>
2023-11-01 16:51:37 +0545

    add test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
     <Link to="/">Home</Link>
     {/* <a href="/addProduct">Add Product</a> */}
     <Link to="/addProduct">Add Product</Link>
+    <Link to="/test">Test</Link>
     {/* <button onClick={()=>navigate("/addProduct")}> Add Product</button> */}
 
 </div>
